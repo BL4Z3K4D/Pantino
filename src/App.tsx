0562f8edf3f3ad7aa36ca6f5a5e5e9450c46ad06
darkh <yazidsaid5@gmail.com>
2026-06-05 12:48:49 -0500

    Add basename to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const AppRoutes = () => {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/Pantino/'>
       <a
         href="#inicio"
         className="sr-only focus:not-sr-only focus:fixed focus:top-4 focus:left-4 focus:z-[100] focus:rounded-lg focus:bg-pantino focus:px-4 focus:py-2 focus:text-white"
